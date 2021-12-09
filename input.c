7c651095cf8c088d7ddc77e2b7d66c2889fd7f18
Donya Johari <djohari37@gmail.com>
2021-12-08 23:33:28 -0500

    Added input file

---
+++ input.c
@@ -0,0 +1,26 @@
+#include "set_up.c"
+
+int flag;
+
+void input()
+{
+    if (kbhit()) {
+        switch (getch()) {
+        case 'w':
+            flag = 4;
+        break;
+        case 'a':
+            flag = 1;
+            break;
+        case 's':
+            flag = 2;
+            break;
+        case 'd':
+            flag = 3;
+            break;
+        case 'z':
+            game_over = 1;
+            break;
+        }
+    }
+}
